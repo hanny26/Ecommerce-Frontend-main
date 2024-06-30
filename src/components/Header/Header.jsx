4a60798d732bc1269d0484ca793ebfec448771f9
hanny26 <hannyvyas109@gmail.com>
2024-06-30 12:26:30 +0530

    text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,10 +11,10 @@ const Header = () => {
         <span className="ml-3 text-xl">TRIDENT</span>
       </a>
       <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-        <a className="mr-5 hover:text-gray-900">First Link</a>
-        <a className="mr-5 hover:text-gray-900">Second Link</a>
-        <a className="mr-5 hover:text-gray-900">Third Link</a>
-        <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+        <a className="mr-5 hover:text-gray-900">Home</a>
+        <a className="mr-5 hover:text-gray-900">About Us</a>
+        {/* <a className="mr-5 hover:text-gray-900"></a>
+        <a className="mr-5 hover:text-gray-900">Fourth Link</a> */}
       </nav>
       <button className="inline-flex items-center bg-green-500 border-0 py-2 px-4 focus:outline-none hover:bg-green-900 text-white rounded-lg text-base mt-4 md:mt-0">Cart: <span className='font-bold text-red-600'> {cart.length}</span>
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
